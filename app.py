d0a7342d7a12ea0ed39056d146822e70d14e5985
arturo tavizon moreno <you@example.com>
2022-02-16 21:44:54 -0600

    ProyectoFinal
    El final de los timepos

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
+from database import tasks
+
 app = Flask(__name__)
 
+#si tu ni le dices a route que tipo de metodo http va usar, por default usará un GET
 @app.route("/")
 def Index():
-    return render_template("index.html")
+  return render_template("index.html", todoList = tasks)
 
-#@app.route("/ebc")
-#def HolaEBC():
-#    return "<a href>Hola ebc</a>"
-
-#@app.route("/ebc/random")
-#def UnGetRandom():
-#    return "Hola EBC Random"
+@app.route("/AddnewTask", methods=['POST'])
+def AddNewTask():
+  if request.method == 'POST':
+    newTask = {"id": len(tasks) + 1, "name": request.form['task_name']}
+    tasks.append(newTask)
+    return redirect(url_for('Index'))
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+  app.run(port=5000, debug=True)
